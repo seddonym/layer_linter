1ab12df4e65bc1b2c8b6572dfde4a9e33ffa3dc0
David Seddon <david.seddon@octoenergy.com>
2018-10-13 17:22:55 +0100

    Corrected invalid Development Status classifier

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author="David Seddon",
     author_email='david@seddonym.me',
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
